229f111f1f5e8d78658681f7cb09221fb2d9cabe
Katherine Mallinson <KatieMallinson@katherines-air.home>
2021-05-25 13:00:45 -0700

    adding test file

---
+++ test.py
@@ -0,0 +1,8 @@
+Hobbies = {
+    "name": "Katie",
+    "age": 37,
+    "hobby": ["Running","Climbing","Reading"]
+    "wake_up": {"Mon_Wed": 5, "Thurs": 4:30}
+
+
+print(f'{Hobbies["name"]} and I have {Counter["hobby"]}. 
