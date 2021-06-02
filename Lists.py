788a1d4a90267dd6d0d9945f9e36f3a7e28493f9
Katherine Mallinson <KatieMallinson@katherines-air.home>
2021-06-01 17:13:45 -0700

    lists and idctionary practice

---
+++ Lists.py
@@ -0,0 +1,31 @@
+# names = ['john', 'katie', 'sarah', 'darrn', 'jeff']
+# print(names[2])
+# #square brackets will provide a new list but does not modify the original list. 
+# names[0] = 'Jon'
+# print(names)
+
+#return highest number in a list
+# numbers = [3,7,2,22,1,4,5]
+# max = numbers[0]
+# for number in numbers:
+#     if number > max:
+#         max = number
+# print(max)
+
+
+# #2D lists
+# matrix = [
+#     [1,2,3],
+#     [4,5,6],
+#     [7,8,9]
+# ]
+# #nested for loop
+# for row in matrix:
+#     for item in row:
+#         print(item)
+
+
+#methods in lists
+numbers = [5,2,1,7,4]
+numbers.remove(5)
+print(numbers)
