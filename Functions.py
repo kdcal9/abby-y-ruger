a8f1eb57fb82ea9e486ed88dd595d4e146d9dc85
Katherine Mallinson <KatieMallinson@katherines-air.home>
2021-06-01 18:32:12 -0700

    added new functions code

--- Functions.py
+++ Functions.py
@@ -7,3 +7,24 @@
 # greet_user("mary")
 # print("Finish")
 
+# functions that return values
+# def square(numbers):
+#     return numbers * numbers
+
+# print(square(3))
+
+# def emoji_converter(message):
+#     words = message.split(' ')
+#     emojis = {
+#         ":)": "😃",
+#         ":(": "🙁"
+#     }
+#     output = ""
+#     for word in words:
+#         output += emojis.get(word, word) + " "
+#     return output
+
+# message = input(">")
+# print(emoji_converter(message))
+
+
--- Lists.py
+++ Lists.py
@@ -25,7 +25,58 @@
 #         print(item)
 
 
-#methods in lists
-numbers = [5,2,1,7,4]
-numbers.remove(5)
-print(numbers)
+#methods in lists, example below is removing duplicates
+# numbers = [5,2,1,7,4,10, 7, 4]
+# uniques = []
+
+# for number in numbers:
+#     if number not in uniques:
+#         uniques.append(number)
+# print(uniques)
+
+# #tuples
+# numbers = (1,2,3)
+# print(numbers[0])
+
+#unpacking
+coordinates = (1,2,3)
+# x = coordinates[0]
+# y = coordinates[1]
+# z = coordinates[2]
+
+#shorthand to acheive the same results - assigning another variable to a item in a tuple or list.
+# x,y,z = coordinates
+# print(x)
+
+#dictionaries
+# customer = {
+#     'name': 'John Smith',
+#     'age': 30,
+#     'is_verified': True
+#     }
+
+# print(customer['name'])
+# print(customer.get('birthday', 'birthday not assigned'))
+
+# phone = input("Phone: ")
+# digits = {
+#     "1" : "one",
+#     "2" : "two",
+#     "3" : "three",
+#     "4" : "four"
+#     }
+# output = ""
+# for character in phone:
+#     output += digits.get(character,  "!") + " "
+# print(output)
+
+# message = input(">")
+# words = message.split(' ')
+# emojis = {
+#     ":)": "😃",
+#     ":(": "🙁"
+# }
+# output = ""
+# for word in words:
+#     output += emojis.get(word, word) + " "
+# print(output)
